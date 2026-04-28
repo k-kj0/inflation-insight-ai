943d32c43021af36af3e9ce969d5cc5d14c00e59
k-kj0 <kavya.kjais@gmail.com>
2026-04-28 16:01:33 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import cloudflare from "@cloudflare/vite-plugin";
+import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
-    TanStackRouterVite(),
+    TanStackRouterVite({ autoCodeSplitting: true }),
     react(),
     tsconfigPaths(),
-    cloudflare(),
+    tailwindcss(),
   ],
-  base: "/inflation-insight-ai/",
 });
