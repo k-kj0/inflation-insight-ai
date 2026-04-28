b21f0b1414262191de5335d94a134c7e488f2834
k-kj0 <kavya.kjais@gmail.com>
2026-04-28 16:04:58 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/inflation-insight-ai/",
   plugins: [
     TanStackRouterVite({ autoCodeSplitting: true }),
     react(),
